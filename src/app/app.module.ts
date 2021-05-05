a50897655af1b6a696b9b96f52e70c9542c54ce0
mortuza19 <sahilmortuza.90@gmail.com>
2021-05-05 17:00:09 +0530

    Added PWA in the application

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -18,6 +18,7 @@ import { EffectsModule } from '@ngrx/effects';
 import { LoginEffect } from './@ngrx/Login-store/login-effects';
 import { AuthGuard } from './auth.guard';
 import { ContainerEffects } from './@ngrx/Container-store/container-effects';
+import { ServiceWorkerModule } from '@angular/service-worker';
 
 @NgModule({
   declarations: [
@@ -36,7 +37,13 @@ import { ContainerEffects } from './@ngrx/Container-store/container-effects';
     ReactiveFormsModule,
     StoreModule.forRoot(reducers, { metaReducers }),
     StoreDevtoolsModule.instrument({ maxAge: 25, logOnly: environment.production }),
-    EffectsModule.forRoot([LoginEffect, ContainerEffects])
+    EffectsModule.forRoot([LoginEffect, ContainerEffects]),
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: environment.production,
+      // Register the ServiceWorker as soon as the app is stable
+      // or after 30 seconds (whichever comes first).
+      registrationStrategy: 'registerWhenStable:30000'
+    })
   ],
 
   bootstrap: [AppComponent],
